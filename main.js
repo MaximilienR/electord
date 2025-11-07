68cbad2ab76606ea385ae544b314e472b6e07db6
[maximilien.roussel] <[ro.maximilien@gmail.com]>
2025-11-07 14:11:03 +0100

    add modal

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ function createWindow() {
   const { dialog } = require('electron');
 const fs = require('fs');
 
-// Exemple pour ton menu
+// menu
 const template = [
     {
         label: 'File',
@@ -48,12 +48,52 @@ const template = [
             },
             { role: 'quit' }
         ]
+        
     },
+    {
+        label: 'Help',
+        submenu: [
+            { label: 'doc' , click: () => { doc(); } },
+            
+        ]
+    }
 ];
 
 
     const menu = Menu.buildFromTemplate(template);
     Menu.setApplicationMenu(menu);
 }
+
+//
+function doc() {
+  dialog.showMessageBox(mainWindow, {
+    type: 'info',
+    title: 'Documentation',
+    message: `Documentation de l’application
+
+Développeur : Maximilien Freelance
+Année de création : 2025
+
+Description
+
+Cette application a été conçue pour permettre la création rapide et intuitive de notes. Elle offre une interface simple et efficace afin de faciliter la prise de notes au quotidien, tout en restant légère et performante.
+
+Technologies utilisées
+
+L’application a été développée avec les technologies suivantes :
+
+HTML5 : pour la structure et le contenu.
+CSS3 : pour le design et le style visuel.
+JavaScript (ES6) : pour les fonctionnalités et l’interactivité.
+Electron.js : pour créer une application desktop multiplateforme.
+
+Objectif
+
+L’objectif principal de cette application est de fournir aux utilisateurs un outil rapide, fiable et facile à utiliser pour la gestion de leurs notes, que ce soit pour un usage personnel ou professionnel.`,
+    buttons: ['OK']
+  });
+}
+;
+
 
 app.whenReady().then(createWindow);
